e66150da3e2e115357b0bed57e5bff15b5b4c54b
katchportal <katchportal.in@gmail.com>
2023-09-08 12:01:41 +0800

    Display stock price, deal with invalid search, set up ajax and javascript for submission

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,12 @@
 class Stock < ApplicationRecord
-
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:publishable_api_key],
                                   secret_token: Rails.application.credentials.iex_client[:secret_api_key],
                                   endpoint: 'https://cloud.iexapis.com/v1')
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name , last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
